3dcda33ee7cf0715803a34ed0a0590f45c6c6c64
Azwie@050317 <23014643@mvula.univen.ac.za>
2023-10-23 00:46:05 +0200

    Swagger config added

---
+++ CodeBits.API/Configuration/MapperConfig.cs
@@ -0,0 +1,12 @@
+﻿using AutoMapper;
+
+namespace CodeBits.API.Configuration
+{
+    public class MapperConfig:Profile
+    {
+        public MapperConfig()
+        {
+
+        }
+    }
+}
--- CodeBits.API/Program.cs
+++ CodeBits.API/Program.cs
@@ -1,3 +1,4 @@
+using CodeBits.API.Configuration;
 using CodeBits.API.Data;
 using CodeBits.API.Entities;
 using CodeBits.API.Repository;
@@ -7,8 +8,12 @@ using FoodStore.Services.AuthAPI.Data;
 using FoodStore.Services.AuthAPI.Models;
 using FoodStore.Services.AuthAPI.Services;
 using FoodStore.Services.AuthAPI.Services.IService;
+using Microsoft.AspNetCore.Authentication.JwtBearer;
 using Microsoft.AspNetCore.Identity;
 using Microsoft.EntityFrameworkCore;
+using Microsoft.IdentityModel.Tokens;
+using Microsoft.OpenApi.Models;
+using System.Text;
 
 var builder = WebApplication.CreateBuilder(args);
 
@@ -29,6 +34,66 @@ builder.Services.AddDbContext<ApplicationDbContext>(option =>
 builder.Services.AddIdentity<AppUser, IdentityRole>().AddEntityFrameworkStores<ApplicationDbContext>()
     .AddDefaultTokenProviders();
 
+//AutoMapper
+builder.Services.AddAutoMapper(typeof(MapperConfig));
+
+//=================================================================================================
+//   SWAGGR GEN AUTHENTICATION
+//=================================================================================================
+builder.Services.AddSwaggerGen(option =>
+{
+    option.AddSecurityDefinition(name: JwtBearerDefaults.AuthenticationScheme, securityScheme: new Microsoft.OpenApi.Models.OpenApiSecurityScheme
+    {
+        Name = "Authorization",
+        Description = "Enter the Bearer Authorization string as following: `Bearer Generated-JWT-Token`",
+        In = ParameterLocation.Header,
+        Type = SecuritySchemeType.ApiKey,
+        Scheme = "Bearer"
+    });
+
+    option.AddSecurityRequirement(new OpenApiSecurityRequirement
+    {
+        {
+            new OpenApiSecurityScheme
+            {
+                Reference= new OpenApiReference
+                {
+                    Type = ReferenceType.SecurityScheme,
+                    Id = JwtBearerDefaults.AuthenticationScheme
+                }
+            },
+            new string[] {}
+        }
+    });
+});
+
+//=================================================================================================
+// Authentication
+//=================================================================================================
+var secret = builder.Configuration.GetValue<string>("ApiSettings:Secret");
+var issuer = builder.Configuration.GetValue<string>("ApiSettings:Issuer");
+var audience = builder.Configuration.GetValue<string>("ApiSettings:Audience");
+
+var key = Encoding.ASCII.GetBytes(secret);
+
+builder.Services.AddAuthentication(x =>
+{
+    x.DefaultAuthenticateScheme = JwtBearerDefaults.AuthenticationScheme;
+    x.DefaultChallengeScheme = JwtBearerDefaults.AuthenticationScheme;
+}).AddJwtBearer(x =>
+{
+    x.TokenValidationParameters = new TokenValidationParameters
+    {
+        ValidateIssuerSigningKey = true,
+        IssuerSigningKey = new SymmetricSecurityKey(key),
+        ValidateIssuer = true,
+        ValidIssuer = issuer,
+        ValidateAudience = true,
+        ValidAudience = audience,
+    };
+});
+builder.Services.AddAuthorization();
+//====================================================================================================
 builder.Services.AddScoped<IJwtTokenGenerator, JwtTokenGenerator>();
 builder.Services.AddScoped<IAuthRepository, AuthRepository>();
 
